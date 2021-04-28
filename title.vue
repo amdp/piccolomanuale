287f4f4f811405c1d468951276401eec1c3cc8d0
Alessandro Merletti De Palo <alessandromerlettidepalo@gmail.com>
2021-04-28 11:49:18 +0200

    nuxt moduling

--- title.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <b-container>
-    <h1>Piccolo Manuale sulla Cooperazione</h1><br><br><br>
-    <h2>a Cura di Alessandro Merletti De Palo</h2><br><br><br><br><br><br>
-    <h1><nuxt-link to="/piccolomanuale/1" style="text-decoration: none;">></nuxt-link></h1>
-    <h5><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons BY-NC-SA International License</a></h5>
-  </b-container>
-<template>
